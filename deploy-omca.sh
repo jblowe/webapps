b75c3ab824c4bacd0d55eb2543de18187f1aaf29
John Lowe <jblowe@berkeley.edu>
2021-02-10 22:55:57 -0800

    upgrade2020: two deployment scripts

---
+++ deploy-omca.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# a helper for deploying the django webapps for omca
+#
+# while it does work, it is really more of an example script...
+# ymmv! use it if it really helps!
+#
+
+VERSION="$1"
+
+# make sure the ucb custom repo is clean and tidy
+cd
+rm -rf omca-clone
+git clone https://github.com/cspace-deployment/cspace-webapps-common omca-clone
+
+
+echo "Deploying OMCA webapps and Solr..."
+cd omca-clone
+~/webapps/setup.sh configure pycharm $VERSION
+~/webapps/setup.sh deploy omca $VERSION
