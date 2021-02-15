ae4d30df6164a2e791058f5372b656e3f7d3abc2
John Lowe <jblowe@berkeley.edu>
2021-02-14 20:04:02 -0800

    upgrade2020: get rid of some more obsolete files

--- etl/utilities/oj.omca.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-~/solr-etl/solrETL-public.sh        omca 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ~/solr-etl/logs/omca.solr_extract_public.log  2>&1
-~/solr-etl/solrdatasources/omca/solrETL-internal.sh      omca 2>&1 | /usr/bin/ts '[%Y-%m-%d %H:%M:%S]' >> ~/solr-etl/logs/omca.solr_extract_internal.log  2>&1
