bc6675f224819ea39d45cd761ca01862334c0814
John Lowe <jblowe@berkeley.edu>
2021-04-25 14:46:51 -0700

    disable analytics gathering

---
+++ omca/apps/cspace_django_site/trackingids.py
@@ -0,0 +1,9 @@
+#
+# UA tracking ids in use for OMCA (none at the moment!)
+#
+#
+
+trackingids = {
+    'omca-prod': ('UA-xxxxxxxx-x', 'OMCA Webapps'),
+    'omca-dev':  ('UA-xxxxxxxx-x', 'OMCA Webapps Development'),
+}
