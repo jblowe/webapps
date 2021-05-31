022f37903b3014d591af8115a069dc8d4be93cc1
John Lowe <jblowe@berkeley.edu>
2021-05-31 16:58:01 -0700

    revise javascript framework: babel and webpack needed help

---
+++ omca/apps/webpack.config.js
@@ -0,0 +1,55 @@
+/* eslint import/no-extraneous-dependencies: "off" */
+
+const webpack = require('webpack');
+
+const library = 'webappstuff';
+/* const env = process.env.NODE_ENV;
+ const isProduction = env === 'production'; */
+const isProduction = true;
+
+const config = {
+    entry: {
+      app: './client_modules/js/app.js'
+    },
+    output: {
+        path: __dirname+ '/webpack_dist/',
+        filename: '[name].bundle.js',
+        library: library,
+        libraryTarget: 'umd'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+            },
+        ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+            $: "jquery",
+            jQuery: "jquery",
+            "window.jQuery": "jquery",
+            d3: 'd3'
+        }),
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx'],
+    },
+};
+
+if (isProduction) {
+    config.plugins.push(
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                warnings: false,
+            },
+            output: {
+                comments: false,
+            },
+        })
+    );
+}
+
+module.exports = config;
