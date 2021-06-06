4f0e912b65d01d8d7bb79bba638cc473716176c1
John Lowe <jblowe@berkeley.edu>
2021-06-06 10:47:13 -0700

    revised 'report list getter'

--- reports/xml/getlistofreports.sh
+++ reports/xml/getlistofreports.sh
@@ -1,2 +1,2 @@
-CREDS=imageserver@museumca.org:digital15%
+eval `grep CREDS  credentials.cfg`
 curl -u ${CREDS} "http://10.99.1.13:8180/cspace-services/reports/?pgSz=100" | xmllint --format > reports.xml -
