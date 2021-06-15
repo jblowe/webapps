ff15333996b931511b728c466fbc947d9e723f33
John Lowe <jblowe@berkeley.edu>
2021-06-14 17:04:46 -0700

    updated report configurations

--- reports/xml/getlistofreports.sh
+++ reports/xml/getlistofreports.sh
@@ -1,2 +1,3 @@
 eval `grep CREDS  credentials.cfg`
-curl -u ${CREDS} "http://10.99.1.13:8180/cspace-services/reports/?pgSz=100" | xmllint --format > reports.xml -
+eval `grep SERVER  credentials.cfg`
+curl -u ${CREDS} "${SERVER}/cspace-services/reports/?pgSz=100" | xmllint --format > reports.xml -
