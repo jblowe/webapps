c2d318878b1192a313dff2b01a78d9a2ff4074dc
John Lowe <jblowe@berkeley.edu>
2021-07-22 19:34:20 -0700

    further revisions to etc/merge_terms_v6.py

--- etc/merge_terms_v6.py
+++ etc/merge_terms_v6.py
@@ -27,16 +27,22 @@ relFailed = []
 
 # retrieve the refname for authority term
 def getrefname(csid, vocab):
-    info = requests.get(baseurl + '/' + vocab + 'authorities/urn:cspace:name('+vocab+')/items/' + csid,
-                        headers=headers, auth=(user, pword))
+    url = baseurl + '/' + vocab + 'authorities/urn:cspace:name(' + vocab + ')/items/' + csid
+    info = requests.get(url, headers=headers, auth=(user, pword))
     refname = re.search('<refName>(.*?)</refName>', info.text)
-    return(refname.group(1))
+    try:
+        return(refname.group(1))
+    except:
+        return None
 
 
 # retrieve the uris of records that use the 'old term' and add them to a list
 def getobjs(oldcsid):
-    usedby = requests.get(baseurl + '/' + termvocab + 'authorities/urn:cspace:name(' + termvocab + ')/items/' + oldcsid +
-                          urlparams, headers=headers, auth=(user, pword))
+    url = baseurl + '/' + termvocab + 'authorities/urn:cspace:name(' + termvocab + ')/items/' + oldcsid + urlparams
+    usedby = requests.get(url, headers=headers, auth=(user, pword))
+    if usedby.status_code != 200:
+        print('Failed to find any related records.')
+        return 0
     for match in re.findall('<uri>(.*?)</uri>', usedby.text):
         to_update.append(match)
         to_update.sort()
@@ -61,7 +67,7 @@ def getRels(csid):
 
 # update list of records replacing 'old term' wth term being merged into and
 # remove core and account info sections of the xml before uploading updated
-# data and reading back a list of any failed attemps
+# data and reading back a list of any failed attempts
 def updateobjs(uri, oldrefname, newrefname):
     record = requests.get(baseurl + uri, headers=headers, auth=(user, pword))
     core = re.search('<ns2:collectionspace_core(.*?)</ns2:collectionspace'
@@ -86,9 +92,9 @@ def delRels(uri):
         relFailed.append(uri)
 
 # remove term that is no longer used
-def deloldterm(csid):
-    remove = requests.delete(baseurl + '/' + termvocab + '/*/items/' + csid,
-                             auth=(user, pword))
+def deloldterm(csid, vocab):
+    url = baseurl + '/' + vocab + 'authorities/urn:cspace:name(' + vocab + ')/items/' + csid
+    remove = requests.delete(url, auth=(user, pword))
     return(remove.status_code)
 
 
@@ -101,25 +107,30 @@ def mergeterms():
     for uri in to_update:
         updateobjs(uri, oldrefname, newrefname)
     if len(failed) == 0:
-        if int(countUsedby) < 1000:
-            print('All records were successfully updated.')
-            if deloldterm(oldcsid) == 200:
-                print(oldcsid + ' successfully deleted.')
-            else:
-                print('Error deleteing term: ' + oldcsid)
+        if int(countUsedby) == 0:
+            print('No record relations found, none deleted.')
+        elif int(countUsedby) < 1000:
+            print('All record relations (if any!) were successfully deleted.')
         else:
-            print(str(len(success)) + ' records successfully updated.'
+            print(str(len(success)) + ' record relations successfully deleted.'
                   ' Re-run merge_terms to update remaining ' +
                   str(int(countUsedby) - len(success)) + ' records')
+        status_code = deloldterm(oldcsid, termvocab)
+        if status_code == 200:
+            print(oldcsid + ' successfully deleted.')
+        elif status_code == 404:
+            print('Not found (already deleted?): ' + oldcsid)
+        else:
+            print('Error deleting term: ' + oldcsid)
     else:
-        print(str(len(success)) + ' records were successfully updated.'
+        print(str(len(success)) + ' record relations were successfully updated.'
               + '\n the following records were not updated:')
         for item in failed:
             print(item)
     for csid in relToDel:
         delRels(csid)
     if len(relFailed) == 0:
-        print('All hierarchy relationships were sucessfully deleted.')
+        print('All hierarchy relationships (if any) were sucessfully deleted.')
     else:
         print('Error deleting the following hierarchy relationships: ' + ', '.join(relFailed))
 
