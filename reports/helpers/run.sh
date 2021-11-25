579944dc21b9e547d842a9d05cf6581bac67438e
John Lowe <jblowe@berkeley.edu>
2021-11-25 13:33:17 -0800

    tidy up documentation and run.sh

--- reports/helpers/run.sh
+++ reports/helpers/run.sh
@@ -9,7 +9,14 @@ time java -jar /home/webapps/RunJasperReports/RunJasperReports.jar \
 # in the webapps users 'jasperreports' directory.
 # it uses an open source repo to execute the jrxml file:
 # https://github.com/okohll/RunJasperReports.git
+
 # to deploy, clone the repo, follow the instructions and place this script in the directory
+# git clone https://github.com/okohll/RunJasperReports.git
+# cd ~/RunJasperReports/
+# cp ~/webapps/reports/helpers/run.sh .
+#### less src/com/gtwm/jasperexecute/RunJasperReports.java
+# ./run.sh Acq_List_Basic.jrxml 9901f41a-4c85-49db-acb5
+
 # the other bit of magic required is the taskrunner webapp task called 'booklets.task'
 # which runs all the booklets reports and makes the result available via the web
 # i.e. it places the pdfs in /var/www/html/reports where they can be retrived by
