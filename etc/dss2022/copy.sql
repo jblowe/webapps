a57e12dd010fae09b6d49c7df6d0ab76f0fdc45e
John Lowe <johnblowe@gmail.com>
2022-09-24 17:31:31 -0700

    tidy up; also use \copy instead of COPY in copy.sql

--- etc/dss2022/copy.sql
+++ etc/dss2022/copy.sql
@@ -1,12 +1,1 @@
-COPY netxview
-    (
-     object_name, accession_number, taxon, phys_desc, ip_status,
-     cult_context, assoc_persons, assoc_place,
-     obj_prod_org, assoc_orgs, prod_date, materials,
-     obj_prod_date, object_title, prod_technique, content_concepts,
-     exhibitions, dimensions, credit_line, moddate
-        )
-    FROM '/Users/johnlowe/webapps/etc/dss2022/netxview.csv'
-    -- FROM '/home/webapps/webapps/etc/dss2022/netxview.csv'
-    DELIMITER E'\t'
-    CSV HEADER
+\COPY netxview ( object_name, accession_number, taxon, phys_desc, ip_status, cult_context, assoc_persons, assoc_place, obj_prod_org, assoc_orgs, prod_date, materials, obj_prod_date, object_title, prod_technique, content_concepts, exhibitions, dimensions, credit_line, moddate ) FROM '/home/webapps/webapps/etc/dss2022/netxview.csv' DELIMITER E'\t' CSV HEADER
