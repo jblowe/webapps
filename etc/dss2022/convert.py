725bd3d12babd95312d2574231986c63f863742c
John Lowe <johnblowe@gmail.com>
2022-12-11 08:33:21 -0800

    modify 'concatenated' fields per Erik's revised spec 12/5/22

--- etc/dss2022/convert.py
+++ etc/dss2022/convert.py
@@ -33,12 +33,24 @@ concatenate_cols = [
 txt_file = sys.argv[1]
 csv_file = sys.argv[2]
 
+def encapsulate(row, cols):
+    # encapsulate items with commas in double quotes, double the double quotes
+    for c in cols:
+        t = row[c].split('|')
+        z = []
+        for x in t:
+            if ',' in x:
+                x = f'"{x}"'
+            z.append(x)
+        row[c] = ','.join(z)
+
 with open(txt_file, "r") as in_text:
     in_reader = csv.reader(in_text, delimiter='\t')
     with open(csv_file, "w") as out_csv:
-        out_writer = csv.writer(out_csv, escapechar='\\', quoting=csv.QUOTE_NONE, delimiter='\t')
+        out_writer = csv.writer(out_csv, escapechar='\\', quoting=csv.QUOTE_MINIMAL, quotechar='"', delimiter='\t')
         for row in in_reader:
             output_row = []
             for cols in concatenate_cols:
-                output_row.append('|'.join([row[c] for c in cols if row[c] != '']))
+                encapsulate(row, cols)
+                output_row.append(','.join([row[c] for c in cols if row[c] != '']))
             out_writer.writerow(output_row)
