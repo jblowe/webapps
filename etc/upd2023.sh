62daccebe25deac8d56abb4fb8c150630d5c6431
John Lowe <johnblowe@gmail.com>
2023-01-02 09:26:21 -0800

    update upd2023.sh so that it will work with .pgpass

--- etc/upd2023.sh
+++ etc/upd2023.sh
@@ -2,7 +2,7 @@
 # run at start of calendar year to update procedure ids to reflect new date - only need to change the year
 
 # media handling
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>MH</initialValue>
@@ -24,7 +24,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = '411d7920-353f-408a-a2ff-3f79db964d5c'"
 
 # location
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>LOC</initialValue>
@@ -46,7 +46,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = '60ac6299-d709-421b-9a62-bb265c34539c'"
 
 # valuation control
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>V</initialValue>
@@ -68,7 +68,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = '962faf0e-4e72-4c36-8756-86956a4a3753'"
 
 # condition check 
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>CC</initialValue>
@@ -90,7 +90,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = '585af100-1a35-11e2-892e-0800200c9a66'"
 
 # restricted media
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>MHR</initialValue>
@@ -121,7 +121,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = 'd49b096b-7b01-4f53-9c49-b437a7e5418e'"
 
 # intake
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>IN</initialValue>
@@ -143,7 +143,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = '8088cfa5-c743-4824-bb4d-fb11b12847f7'"
 
 # conservation treatment 
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>CT</initialValue>
@@ -165,7 +165,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = 'aad54202-404d-4f19-ada9-8b1e378ad1b2'"
 
 # loan in
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>LI</initialValue>
@@ -187,7 +187,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = '8adfb11a-009b-4d13-969b-5ee6e4c47446'"
 
 # intake object
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>IN</initialValue>
@@ -218,7 +218,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = '50596d48-cca9-405a-86c3-aecf3fb74dcb'"
 
 # object exit
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>OE</initialValue>
@@ -240,7 +240,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = 'd4d5e989-b63b-49bb-b769-e24f02ac2ef0'"
 
 # inventory
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>INV</initialValue>
@@ -262,7 +262,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = '0ac91666-0c6e-4d71-b29d-91ae9be786e4'"
 
 # loan out
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>LO</initialValue>
@@ -284,7 +284,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = 'd02966b6-dfd9-452b-ae6b-0a476e52ec42'"
 
 # acquisition
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.YearIDGeneratorPart>
       <currentValue>2023</currentValue>
@@ -302,7 +302,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = '9dd92952-c384-44dc-a736-95e435c1759c'"
 
 # loan in object
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.StringIDGeneratorPart>
       <initialValue>LI</initialValue>
@@ -333,7 +333,7 @@ psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id 
 </org.collectionspace.services.id.SettableIDGenerator>' WHERE csid = 'c9366eed-8207-4d06-b55a-c9846dece67f'"
 
 # object 
-psql omca_domain_omca -U csadmin -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
+psql omca_domain_omca -U csadmin -h localhost -c "UPDATE id_generators SET last_generated_id = '', id_generator_state = '<org.collectionspace.services.id.SettableIDGenerator>
   <parts>
     <org.collectionspace.services.id.YearIDGeneratorPart>
       <currentValue>2023</currentValue>
