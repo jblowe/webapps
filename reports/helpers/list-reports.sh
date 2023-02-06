fe2a52509cd85adc0f089f07b87c623e5a91f7a7
John Lowe <johnblowe@gmail.com>
2023-02-05 20:45:57 -0800

    revised order of fields displayed in list-reports.sh

--- reports/helpers/list-reports.sh
+++ reports/helpers/list-reports.sh
@@ -7,5 +7,5 @@ if [ "$REPORTURL" == "" ] || [ "$REPORTUSER" == "" ]; then
 fi
 
 curl -s -S --stderr - --basic -u "$REPORTUSER" -X GET -H "Content-Type:application/xml" "$REPORTURL/cspace-services/reports?pgSz=1000" > curl.xml
-perl -pe 's/<list/\n<list/g' curl.xml | perl -ne 'while (s/<list\-item>.*?<csid>(.*?)<.*?<(file)?name>(.*?)<.*?<\/list\-item>//) { print "$1\t$3\n" }' 
+perl -pe 's/<list/\n<list/g' curl.xml | perl -ne 'while (s/<list\-item>.*?<csid>(.*?)<.*?<name>(.*?)<.*?<filename>(.*?)<.*?<\/list\-item>//) { print "$2\t$1\t$3\n" }'
 rm curl.xml
