ecba6f5f13a26f15a79079b925fc9e9cee4aa5a9
John Lowe <johnblowe@gmail.com>
2023-05-13 19:34:40 -0700

    update numbers_and_names task to include all names

--- tasks/numbers_and_names.sql
+++ tasks/numbers_and_names.sql
@@ -1,8 +1,12 @@
 SELECT
   coc.objectnumber AS objectnumber_s,
-  regexp_replace(ong.objectname, '^.*\)''(.*)''$', '\1') AS objectname_s
+  --ong.objectname objectname,
+  array_to_string(array_agg(DISTINCT regexp_replace(ong.objectname, '^.*\)''(.*)''$', '\1')),';;') AS objectname_ss
 
 FROM collectionobjects_common coc
   JOIN misc ON (coc.id = misc.id AND misc.lifecyclestate <> 'deleted')
-  LEFT OUTER JOIN hierarchy h2 ON (coc.id=h2.parentid AND h2.name='collectionobjects_common:objectNameList' AND h2.pos=0)
+
+  LEFT OUTER JOIN hierarchy h2 ON (coc.id=h2.parentid AND h2.name='collectionobjects_common:objectNameList')
   LEFT OUTER JOIN objectnamegroup ong ON (ong.id=h2.id)
+
+GROUP BY objectnumber_s
