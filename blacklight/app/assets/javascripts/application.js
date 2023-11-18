ac76f8f2930e0fcc1d1bda10d08f658a37bd9f5a
John Lowe <johnblowe@gmail.com>
2023-11-18 14:46:55 -0800

    initial commit of blacklight customization for omca

---
+++ blacklight/app/assets/javascripts/application.js
@@ -0,0 +1,15 @@
+//= require jquery3
+//= require rails-ujs
+//= turbolinks
+//
+// Required by Blacklight
+//= require popper
+// Twitter Typeahead for autocomplete
+//= require twitter/typeahead
+//= require bootstrap
+//= require blacklight/blacklight
+//= require blacklight_gallery
+
+//= require 'blacklight_range_limit'
+//= require 'blacklight_advanced_search'
+
