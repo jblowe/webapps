348cd8a34abab370136955af9c12156b7766d2f9
John Lowe <johnblowe@gmail.com>
2023-11-17 18:27:04 -0800

    media.sql now respects approveforpublic AND orders by media id number

--- etl/media.sql
+++ etl/media.sql
@@ -11,11 +11,13 @@ mc.copyrightstatement,
 mc.identificationnumber,
 mc.rightsholder rightsholderRefname,
 mc.rightsholder rightsholder,
-mc.contributor
+mc.contributor,
+mo.approveforpublic
 
 FROM media_common mc
 
 JOIN misc ON (mc.id = misc.id AND misc.lifecyclestate <> 'deleted')
+JOIN media_omca mo on (mc.id = mo.id)
 LEFT OUTER JOIN hierarchy h1 ON (h1.id = mc.id)
 INNER JOIN relations_common r on (h1.name = r.objectcsid)
 LEFT OUTER JOIN hierarchy h2 on (r.subjectcsid = h2.name)
@@ -23,5 +25,7 @@ LEFT OUTER JOIN collectionobjects_common cc on (h2.id = cc.id)
 
 JOIN hierarchy h3 ON (mc.blobcsid = h3.name)
 LEFT OUTER JOIN blobs_common b on (h3.id = b.id)
+
+WHERE mo.approveforpublic
 
 ORDER BY cc.objectnumber DESC
