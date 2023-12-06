1d6902533763f9d0365c977c8db8133791a22e07
John Lowe <johnblowe@gmail.com>
2023-12-06 11:47:20 -0800

    testSolr.py works better, and runs in python3

--- etl/testSolr.py
+++ etl/testSolr.py
@@ -4,6 +4,8 @@ import solr
 s = solr.SolrConnection('http://localhost:8983/solr/omca-public')
 
 # do a search
-response = s.query('objname_txt:canoe')
+response = s.query('text:canoe')
 for hit in response.results:
-    print hit['objmusno_s']
+    print(hit)
+    print(hit['objectnumber_s'])
+    print(hit['blob_ss'])
