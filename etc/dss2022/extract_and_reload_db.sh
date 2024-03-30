e581994ecc0bdacff36eb6553aa9a642ece44e89
John Lowe <johnblowe@gmail.com>
2024-03-30 12:40:56 -0700

    update netx data sync extract after solr schema changes

--- etc/dss2022/extract_and_reload_db.sh
+++ etc/dss2022/extract_and_reload_db.sh
@@ -6,8 +6,9 @@ cd ~webapps/webapps/etc/dss2022
 source pgvars.sh
 echo "copying 4solr.omca.public.csv.gz and extracting and massaging columns..."
 cp ~webapps/solr-pipelines/4solr.omca.public.csv.gz .
-gunzip -f 4solr.omca.public.csv.gz 
-cut -f 4,3,7,33,74,28,34,52,15,16,30,31,40,42,26,43,25,41,27,32,18,24,44,20,58,23 4solr.omca.public.csv > netx-extract.csv
+gunzip -f 4solr.omca.public.csv.gz
+# nb: even though the columns are listed out of order, cut outputs them in order. caveat coder!
+cut -f 3,4,6,15,16,18,22,26,27,28,29,30,31,33,34,35,36,37,43,44,45,46,47,55,62,78 4solr.omca.public.csv > netx-extract.csv
 python convert.py netx-extract.csv netxview.csv
 echo "`wc -l netxview.csv` rows (including header) extracted from '4solr file' containing `wc -l 4solr.omca.public.csv | cut -f1 -d" "` lines"
 psql -f create-netxview.sql
