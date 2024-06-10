7e931c6d761336fc7229fb15c809c82a4aa05fe5
John Lowe <johnblowe@gmail.com>
2024-06-09 18:05:58 -0700

    small revision to solr pipeline deployment script

--- deploy-omca-webapps.sh
+++ deploy-omca-webapps.sh
@@ -13,8 +13,8 @@ OMCA_VERSION="$1"
 echo "Deploying OMCA Solr pipelines"
 YYYYMMDDHHMM=`date +%Y%m%d%H%M`
 mv solr-pipelines ${YYYYMMDDHHMM}.solr-pipelines
-cp -r ~/webapps/etl ~/solr-pipelines
-cp solr-pipelines/utilities/checkstatus.sh ~
+cp -r ~/webapps/etl/* ~/solr-pipelines
+cp ~/webapps/etl/utilities/checkstatus.sh ~
 
 echo "Deploying OMCA webapps version ${OMCA_VERSION}"
 ~/webapps/setup.sh deploy omca prod latest "${OMCA_VERSION}"
