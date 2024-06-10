895c5758f9009439a70f33a632667bb30ac6a2a1
John Lowe <johnblowe@gmail.com>
2024-06-09 18:44:26 -0700

    clarify revisions to solr pipeline deployment script

--- deploy-omca-webapps.sh
+++ deploy-omca-webapps.sh
@@ -11,10 +11,13 @@ cd
 OMCA_VERSION="$1"
 
 echo "Deploying OMCA Solr pipelines"
+# nb: this moves the solr working directory and recreates it from
+# scratch. simple, but do be careful if you change how it works.
 YYYYMMDDHHMM=`date +%Y%m%d%H%M`
 mv solr-pipelines ${YYYYMMDDHHMM}.solr-pipelines
-cp -r ~/webapps/etl/* ~/solr-pipelines
+cp -r ~/webapps/etl ~/solr-pipelines
 cp ~/webapps/etl/utilities/checkstatus.sh ~
 
 echo "Deploying OMCA webapps version ${OMCA_VERSION}"
+# 'latest' means to deploy the latest release tag
 ~/webapps/setup.sh deploy omca prod latest "${OMCA_VERSION}"
