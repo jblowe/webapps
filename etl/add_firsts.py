896f693a773528ab264459027c39450473448a56
John Lowe <johnblowe@gmail.com>
2024-06-10 18:26:45 -0700

    solr pipeline add_firsts.py needs to open input w/o newline

--- etl/add_firsts.py
+++ etl/add_firsts.py
@@ -26,7 +26,7 @@ errors = 0
 
 with open(sys.argv[2], 'w') as f2:
     writer = csv.writer(f2, delimiter=delim, quoting=csv.QUOTE_NONE, quotechar=chr(255), escapechar='\\')
-    with open(sys.argv[1], 'r') as f1:
+    with open(sys.argv[1], 'r', newline='') as f1:
         reader = csv.reader(f1, delimiter=delim, quoting=csv.QUOTE_NONE, quotechar=chr(255))
         for lineno, row in enumerate(reader):
             if lineno == 0:
