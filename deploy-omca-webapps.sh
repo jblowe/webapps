11c91685d5a62b5d29a46e384f611248b285f869
John Lowe <johnblowe@gmail.com>
2024-06-11 19:03:04 -0700

    factor webapps/solr deploy script so that the solr part respects versioning

--- deploy-omca-webapps.sh
+++ deploy-omca-webapps.sh
@@ -10,14 +10,15 @@ cd
 
 OMCA_VERSION="$1"
 
+echo "Deploying OMCA webapps version ${OMCA_VERSION}"
+# 'latest' means to deploy the latest release tag
+~/webapps/setup.sh deploy omca prod latest "${OMCA_VERSION}"
+
 echo "Deploying OMCA Solr pipelines"
+set -x
 # nb: this moves the solr working directory and recreates it from
 # scratch. simple, but do be careful if you change how it works.
 YYYYMMDDHHMM=`date +%Y%m%d%H%M`
 mv solr-pipelines ${YYYYMMDDHHMM}.solr-pipelines
 cp -r ~/webapps/etl ~/solr-pipelines
 cp ~/webapps/etl/utilities/checkstatus.sh ~
-
-echo "Deploying OMCA webapps version ${OMCA_VERSION}"
-# 'latest' means to deploy the latest release tag
-~/webapps/setup.sh deploy omca prod latest "${OMCA_VERSION}"
