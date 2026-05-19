1ba5a01469b47532ccab711c34f607caa62cd890
John B. Lowe <jblowe@berkeley.edu>
2026-05-19 16:07:11 -0700

    bulletproof blacklight portal solr refresh script a bit

--- etl/utilities/repost-to-solr.sh
+++ etl/utilities/repost-to-solr.sh
@@ -10,7 +10,15 @@ set -e
 cd
 
 echo "Refreshing OMCA Solr core $(date)"
-gunzip -c -f 4solr.omca.public.csv.gz > 4solr.omca.public.csv
+gunzip -c -f 4solr.omca.public.csv.gz > 4solr.omca.public.csv || { echo "ERROR: could not gunzip 4solr.omca.public.csv.gz" >&2; exit 1; }
+
+lines=$(wc -l < 4solr.omca.public.csv)
+# only reload the .csv file if it has more than 300,000 lines (i.e. a full file)
+if [[ $lines -le 300000 ]]; then
+    echo "ERROR: 4solr.omca.public.csv has only $lines line(s), aborting." >&2
+    exit 1
+fi
+
 # || {die 'could not gunzip' ; exit 1;}
 time ./solr-curl.sh
 echo "done $(date)"
